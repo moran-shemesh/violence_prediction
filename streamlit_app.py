8fa765676e309f5fbdc5c4a430ca402c8e9acbe6
Moran <56132711+moran-shemesh@users.noreply.github.com>
2023-09-13 14:21:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,11 @@ st.caption(" © Violence Prediction by Moran Shemesh")
 user_link = st.text_area("", placeholder="Add here a youTube link to a video")
 
 
+def download1(link, video_path, file_num):
+  name = str(file_num)
+  try:
+    !yt-dlp -f "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" $link -o "/content/drive/MyDrive/HIT/whatsapp_analyze/data_03_07_2023/mp4/%(title)s"
+  except:
+    print("An error has occurred")
+  print("Download is completed successfully")
+
