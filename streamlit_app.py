f9e12371456370a5705d4ce9f10efcb4a65d0cd2
Moran <56132711+moran-shemesh@users.noreply.github.com>
2023-09-13 14:12:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 # https://moran-shemesh-abuser-streamlit-app-oyisjd.streamlit.app/
 # https://www.datacamp.com/tutorial/streamlit
 import streamlit as st
-
+from pytube import YouTube
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+import os
 
 #OPENAI_API_KEY = config('KEY') #take the key from the .env file
 #translator = Translator()
