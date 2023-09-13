b3dafae96e029643511991cc8d2011088f462d9b
Moran <56132711+moran-shemesh@users.noreply.github.com>
2023-09-13 16:04:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# The main flow should be here
+def main():
+    # test_data()
+
+    # test_clean_data()
+
+    # test_textrank()
+
+    # test_evaluate()
+
+    # test_evaluate_textrank_on_clean_test_data()
+
+    # result: no error:
+    # test_build_pretrained_bart()
+
+    # result: 1, 1
+    # test_bart_data_blocks()
+
+    # result: print about training BART
+    # test_bart_train()
+
+    # result: ground-truth summary and generated summary
+    # test_local_bart_summary()
+
+    # print generated summary
+    
+
+
+if __name__ == "__main__":
+    main()
+  # The main flow should be here
