cb476ca323c3e56890fc19b1baac97d308eb108d
Moran <56132711+moran-shemesh@users.noreply.github.com>
2023-09-13 21:47:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 import os
-
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
 #OPENAI_API_KEY = config('KEY') #take the key from the .env file
 #translator = Translator()
 
@@ -18,5 +19,18 @@ st.caption(" © Violence Prediction by Moran Shemesh")
 
 user_link = st.text_area("", placeholder="Add here a youTube link to a video")
 
+def download(link, video_path):
+  print("1")
+  youtubeObject = YouTube(link)
+  print("2")
+  youtubeObject = youtubeObject.streams.get_highest_resolution()
+  print("3")
+  name = "video"
+  print(name)
+  try:
+      youtubeObject.download(output_path= video_path, filename=name)
+  except:
+      print("An error has occurred")
+  print("Download is completed successfully")
 
 
